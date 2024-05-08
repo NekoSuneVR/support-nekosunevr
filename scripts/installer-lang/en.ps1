82c76b425f7ea00fca5376b48113c5c5b681ca84
NekoSuneVR <Chisdeal2014@hotmail.com>
2024-05-08 14:30:56 +0100

    Add files via upload

---
+++ scripts/installer-lang/en.ps1
@@ -0,0 +1,17 @@
+[PSCustomObject]@{
+    Welcome         = "
+╔════════════════════════════════════════════════════╗
+║ Welcome to NekoSuneVR Supporter System for Windows ║
+╚════════════════════════════════════════════════════╝"
+    Download        = "Error downloading"
+    Download2       = "Will re-request in 5 seconds..."
+    Download3       = "Error again"
+    Download4       = "Check your network settings and run the installation again"
+    Download5       = "Downloading Support Tools"
+    Error           = "Error"
+    InstallComplete = "installation completed"
+    HostInfo        = "Unwanted URLs found in hosts file"
+    HostBak         = "Backing up hosts.bak..."
+    HostDel         = "Trying to remove unwanted URLs from the original hosts file..."
+    HostError       = "Something went wrong while editing the hosts file, edit it manually or run the script as administrator"
+}
