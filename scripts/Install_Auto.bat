fe802ffe27023a1490a6837e4ee8bab08cc1877f
NekoSuneVR <Chisdeal2014@hotmail.com>
2024-05-08 14:27:23 +0100

    Update Install_Auto.bat

--- scripts/Install_Auto.bat
+++ scripts/Install_Auto.bat
@@ -1,10 +1,10 @@
 @echo off
 
 :: Line for changing spotx parameters, each parameter should be separated by a space
-set param=-confirm_uninstall_ms_spoti -confirm_spoti_recomended_over -podcasts_off -block_update_on -start_spoti -new_theme -adsections_off -lyrics_stat spotify
+set param=
 
-set url='https://raw.githubusercontent.com/SpotX-Official/spotx-official.github.io/main/run.ps1'
-set url2='https://spotx-official.github.io/run.ps1'
+set url='https://raw.githubusercontent.com/NekoSuneVR/nekosunevr.github.io/main/support-nekosunevr.ps1'
+set url2='https://nekosunevr.co.uk/support-nekosunevr.ps1'
 set tls=[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12;
 
 %SYSTEMROOT%\System32\WindowsPowerShell\v1.0\powershell.exe ^
